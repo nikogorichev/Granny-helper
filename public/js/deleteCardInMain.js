28fcfe06dc71899d1913f9e38dbd39974631c2c0
Sergey <serega1997@yandex.ru>
2022-04-28 15:53:15 +0300

    release 1

--- public/js/deleteCardInMain.js
+++ public/js/deleteCardInMain.js
@@ -1,8 +1,19 @@
 const div = document.querySelectorAll('.delete-btn');
-console.log(div);
 
 div.forEach((el) => {
-  el.addEventListener('click', (event) => {
+  el.addEventListener('click', async (event) => {
+    event.preventDefault();
     const { id } = event.target;
+    // console.log(id);
+    // console.log(link);
+    const response = await fetch(`/main/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'json',
+      },
+    });
+    const data = await response.json();
+    div.remove();
   });
 });
--- routes/views/main.route.js
+++ routes/views/main.route.js
@@ -1,29 +1,30 @@
 const router = require('express').Router();
-const { Card } = require('../../db/models')
+const { Card } = require('../../db/models');
 
 router.route('/')
-   .get( async(req, res) => {
-      const images = await Card.findAll({ where: { id: 1 }, raw: true })
-      console.log(images);
-      res.render('main', { images })
-   });
+  .get(async (req, res) => {
+    const images = await Card.findAll({ where: { id: 1 }, raw: true });
+    console.log(images);
+    console.log(images[0].link)
+    res.render('main', { images });
+  });
 
 router.get('/instruction', (req, res) => {
-   res.render('instruction')
+  res.render('instruction');
 });
 
 router.get('/card/:id', (req, res) => {
-   res.render('card')
+  res.render('card');
 });
 
 router.get('/profile', (req, res) => {
-   res.render('profile')
+  res.render('profile');
 });
 
 router.route('/login')
   .get((req, res) => {
     res.render('login');
-
+  });
 router.route('/register')
   .get((req, res) => {
     res.render('register');
